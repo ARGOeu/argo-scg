09ca7f70e08f717fe23caf5d90be8c5e597a81b9
Katarina Zailac <katarina.zailac@gmail.com>
2023-02-22 15:00:31 +0100

    Extend scg-run-check tool to actually run the requested command

--- exec/scg-run-check.py
+++ exec/scg-run-check.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import argparse
+import json
 
+import requests
 from argo_scg.config import Config
 from argo_scg.exceptions import SensuException, ConfigException
+from argo_scg.generator import generate_adhoc_check
 from argo_scg.sensu import Sensu
 
 CONFFILE = "/etc/argo-scg/scg.conf"
@@ -26,6 +29,9 @@ def main():
         "--config", type=str, dest="config", help="configuration file",
         default=CONFFILE
     )
+    parser.add_argument(
+        "--execute", dest="execute", action="store_true", help="run the command"
+    )
     args = parser.parse_args()
 
     try:
@@ -34,11 +40,55 @@ def main():
         token = config.get_sensu_token()
 
         sensu = Sensu(url=url, token=token)
-        print(
-            sensu.get_check_run(
-                entity=args.entity, check=args.check, namespace=args.namespace
-            )
+        command = sensu.get_check_run(
+            entity=args.entity, check=args.check, namespace=args.namespace
         )
+
+        if args.execute:
+            subscriptions = sensu.get_check_subscriptions(
+                check=args.check, namespace=args.namespace
+            )
+            check = generate_adhoc_check(
+                command=command, subscriptions=subscriptions,
+                namespace=args.namespace
+            )
+            sensu.put_check(check=check, namespace=args.namespace)
+
+            response = requests.post(
+                f"{url}/api/core/v2/namespaces/{args.namespace}/checks/"
+                f"{check['metadata']['name']}/execute",
+                headers={
+                    "Authorization": f"Key {token}",
+                    "Content-Type": "application/json"
+                },
+                data=json.dumps({"check": check["metadata"]["name"]})
+            )
+
+            if response.ok:
+                agent = sensu.get_agents(namespace=args.namespace)[0]
+                print(f"Executing command:\n{command}\n")
+                print(
+                    sensu.get_event_output(
+                        entity=agent["metadata"]["name"],
+                        check=check["metadata"]["name"],
+                        namespace=args.namespace
+                    )
+                )
+                sensu.delete_event(
+                    entity=agent["metadata"]["name"],
+                    check=check["metadata"]["name"],
+                    namespace=args.namespace
+                )
+                sensu.delete_check(
+                    check=check["metadata"]["name"],
+                    namespace=args.namespace
+                )
+
+            else:
+                print(f"{args.namespace}: Error executing ad-hoc check")
+
+        else:
+            print(f"Executing command:\n{command}")
 
     except ConfigException as e:
         print(e)
